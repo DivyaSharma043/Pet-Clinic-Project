faea0af113fcf1665097c9f614d8fd478f0ad77f
DIVYA SHARMA <divya@racloop.com>
2021-11-09 10:59:36 +0530

    pet clinic project

---
+++ pet-clinic-web/src/test/java/petclinicproject/PetClinicProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package petclinicproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetClinicProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
